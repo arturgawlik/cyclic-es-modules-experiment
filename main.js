38930f4f19551e38590e4e4ebd53bb694614248c
Artur Gawlik <agawlik@inpost.pl>
2024-07-31 15:37:37 +0200

    init

---
+++ main.js
@@ -0,0 +1,2 @@
+import "./module-1.js";
+import "./module-2.js";
---
+++ module-1.js
@@ -0,0 +1,4 @@
+import { SOME_CONST_2 } from "./module-2.js";
+
+export var SOME_CONST_1 = "HELLO FROM CONST 1";
+console.log(SOME_CONST_2);
